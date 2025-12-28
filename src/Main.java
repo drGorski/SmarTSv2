716706ac2abe44d52f2036dff2c6ea825f9abb24
drGorski <czasnagdynie@gmail.com>
2025-12-28 14:08:01 +0100

    Actions.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int x = 1;
+        // second variable
+        int y = 1;
+        x = x + y;
     }
 }
